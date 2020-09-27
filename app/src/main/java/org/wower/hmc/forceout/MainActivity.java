5bb03f51c5b545f67848cb9223992d38726b6274
Humengchao <810244965@qq.com>
2020-09-27 20:56:17 +0800

    通过SharedPreferences添加了保存密码的功能

--- app/src/main/java/org/wower/hmc/forceout/MainActivity.java
+++ app/src/main/java/org/wower/hmc/forceout/MainActivity.java
@@ -3,10 +3,13 @@ package org.wower.hmc.forceout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,6 +20,10 @@ public class MainActivity extends BaseActivity {
 
     private static final String TAG = "MainActivity";
 
+    private SharedPreferences.Editor editor;
+    private SharedPreferences pref;
+    private CheckBox rememberPass;
+
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
@@ -26,9 +33,21 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+
         accountEdit = (EditText) findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember){
+            // 将账号密码设置到文本框中
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +57,16 @@ public class MainActivity extends BaseActivity {
                 password = passwordEdit.getText().toString();
                 // 如果账号是admin，密码是123456,就认为登录成功
                 if (account.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()){
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        // 如果没有选择保存密码，则把保存在SharedPreferences里的账号密码删了
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent = new Intent(MainActivity.this, LoginSuccess.class);
                     startActivity(intent);
                     finish();
